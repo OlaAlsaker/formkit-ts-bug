896fb299d2f90c7b4297d15c7607f7237413b0c2
Ola Alsaker <ola.alsaker@appex.no>
2024-04-12 01:43:52 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <FormKit type="form" @submit="handleSubmit">
+      <FormKit type="text" name="title" label="Title" />
+    </FormKit>
+  </div>
+</template>
+
+<script setup lang="ts">
+function handleSubmit(data: any, node: FormKitNode) {
+  console.log("Data", data);
+}
+</script>
---
+++ formkit.config.ts
@@ -0,0 +1,11 @@
+import { defineFormKitConfig } from "@formkit/vue";
+import {
+  createMultiStepPlugin,
+  createAutoAnimatePlugin,
+} from "@formkit/addons";
+
+export default defineFormKitConfig({
+  // rules: {},
+  // locales: {},
+  // etc.
+});
